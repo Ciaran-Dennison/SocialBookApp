bd94832860a874b395894ae3cfa1ecc2c6b597f8
Ciaran Dennison <ciaran.dennison@audacia.co.uk>
2026-06-16 16:59:21 +0100

    feat: configure EF Core DbContext and DI registration #4

--- SocialBookAppCLI/Program.cs
+++ SocialBookAppCLI/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SocialBookAppApplication;
+using SocialBookAppInfrastructure;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var services = new ServiceCollection();
+
+services.AddDbContext<SocialBookAppContext>(options =>
+    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+services.AddScoped<IRatingService, RatingService>();
+services.AddScoped<IUserService, UserService>();
+services.AddScoped<IBookService, BookService>();
+services.AddScoped<IAuthorService, AuthorService>();
+services.AddScoped<IReviewService, ReviewService>();
+
+var serviceProvider = services.BuildServiceProvider();
---
+++ SocialBookAppInfrastructure/SocialBookAppContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SocialBookAppDomain;
+
+namespace SocialBookAppInfrastructure;
+
+public class SocialBookAppContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Publisher> Publishers { get; set; }
+    public DbSet<Author> Authors { get; set; }
+
+    public SocialBookAppContext(DbContextOptions<SocialBookAppContext> options) : base(options)
+    {
+
+    }
+}
